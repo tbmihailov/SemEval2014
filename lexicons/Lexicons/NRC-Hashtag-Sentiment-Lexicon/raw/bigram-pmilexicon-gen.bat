56f7688a2a86a19bbea00409de8ef52921564715
Todor B. Mihaylov <tbmihailov@gmail.com>
2014-03-23 01:33:26 +0200

    Added BingLiu-Lexicon - Bing Liu's opinion lexicon as Gazetteer: http://www.cs.uic.edu/~liub/FBS/opinion-lexicon-English.rar

--- lexicons/Lexicons/NRC-Hashtag-Sentiment-Lexicon/raw/bigram-pmilexicon-gen.bat
+++ lexicons/Lexicons/NRC-Hashtag-Sentiment-Lexicon/raw/bigram-pmilexicon-gen.bat
@@ -1,1 +1,1 @@
-LexiconToGazetteer bigrams-pmilexicon.txt "(.+)\t(.+)\t(.+)\t(.+)" "$1	sentimentScore=$2	numPositive=$3	numNegative=$4" bigrams-pmilexicon.lst
+LexiconToGazetteer bigrams-pmilexicon.txt "(.+)\t(.+)\t(.+)\t(.+)" "$1	nrcSen_bigram-sentimentScore=$2	nrcSen_bigram-numPositive=$3	nrcSen_bigram-numNegative=$4" bigrams-pmilexicon.lst
