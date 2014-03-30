b38f33b02b501f02cbc2d302e8034646f880a218
Yasen Kiprov <yasen@yasen.sentimentmetrics.com>
2014-03-31 00:24:35 +0300

    added multiple = escape

--- baseline/groovy/export.groovy
+++ baseline/groovy/export.groovy
@@ -1,54 +1,55 @@
 // export to file
 new File(doc.name).withWriterAppend{ out ->
-	doc.getAnnotations().get("tweet").each{ tw ->
-		String t = "\t"
-		
-		// export label
-		if(tw.features.label != null) {
-			out.write(tw.features.label + t);
-		}
-		
-		// export tweet
-		out.write(doc.stringFor(tw) + t);
-		
-		Map<String, Integer> features = new HashMap<String, Integer>();
-		
-		tw.features.each{ fName, fValue ->
-			if(!fName.equals("label")) {
-				if(fValue instanceof Collection && !(fValue instanceof String)) {
-					
-					fValue.each{ val ->
-					//	out.write(val.toString() + t);
-						if(features.containsKey(val)) {
-							int i = features.get(val);
-							++i;
-							features.put(val, i);
-						} else {
-							features.put(val, 1);
-						}
-					}
-				} else {
-					String newFeat = fName + "=" + fValue.toString();
-				//	out.write(newFeat + t);
-					
-					if(features.containsKey(newFeat)) {
-						int i = features.get(newFeat);
-						++i;
-						features.put(newFeat, i);
-					} else {
-						features.put(newFeat, 1);
-					}
-				}
-			}
-		}
-		
-		features.each{ fName, fValue ->
-			if(fValue.intValue() == 1) {
-				out.write(fName + t);
-			} else {
-				out.write(fName + "=" + String.valueOf(fValue) + t);
-			}
+    doc.getAnnotations().get("tweet").each{ tw ->
+        String t = "\t"
+        
+        // export label
+        if(tw.features.label != null) {
+            out.write(tw.features.label + t);
         }
-		out.write("\n");
-	}
+        
+        // export tweet
+        out.write(doc.stringFor(tw) + t);
+        
+        Map<String, Integer> features = new HashMap<String, Integer>();
+        
+        tw.features.each{ fName, fValue ->
+            if(!fName.equals("label")) {
+                if(fValue instanceof Collection && !(fValue instanceof String)) {
+                    
+                    fValue.each{ val ->
+                    //    out.write(val.toString() + t);
+                        if(features.containsKey(val)) {
+                            int i = features.get(val);
+                            ++i;
+                            features.put(val, i);
+                        } else {
+                            features.put(val, 1);
+                        }
+                    }
+                } else {
+                    String newFeat = fName + "=" + fValue.toString();
+                //    out.write(newFeat + t);
+                    
+                    if(features.containsKey(newFeat)) {
+                        int i = features.get(newFeat);
+                        ++i;
+                        features.put(newFeat, i);
+                    } else {
+                        features.put(newFeat, 1);
+                    }
+                }
+            }
+        }
+        
+        features.each{ fName, fValue ->
+            if(fValue.intValue() == 1) {
+                out.write(fName + t);
+            } else {
+                String cleanName = fName.replace("=", "eq.");
+                out.write(cleanName + "=" + String.valueOf(fValue) + t);
+            }
+        }
+        out.write("\n");
+    }
 }
